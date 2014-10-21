9eaaed4d2ccb9a0ab3d457f1302dcc16ff8b114f
Yonghong Yan <yanyh15@gmail.com>
2014-10-21 15:20:10 -0400

    organize axpy folder and test GPU support

--- axpy/axpy.h
+++ axpy/axpy.h
@@ -3,7 +3,9 @@
 
 /* change this to do saxpy or daxpy : single precision or double precision*/
 #define REAL double
-#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
 #include "homp.h"
 
 #ifdef __cplusplus
@@ -11,9 +13,7 @@ extern "C" {
 #endif
 
 /* both the omp version and ompacc version */
-extern void axpy_omp(REAL* x, REAL* y,  long n, REAL a); 
-extern void axpy_ompacc(REAL* x, REAL* y,  long n, REAL a); 
-extern void axpy_ompacc_mdev_1(REAL* x, REAL* y,  long n, REAL a);
+extern void axpy(REAL* x, REAL* y,  long n, REAL a); 
 extern double axpy_ompacc_mdev_v2(REAL* x, REAL* y,  long n, REAL a);
 #ifdef __cplusplus
  }
--- axpy/bkp/axpy.h
+++ axpy/bkp/axpy.h
@@ -3,8 +3,7 @@
 
 /* change this to do saxpy or daxpy : single precision or double precision*/
 #define REAL double
-#include <stdlib.h>
-#include <pthread.h>
+#include <omp.h>
 #include "homp.h"
 
 #ifdef __cplusplus
@@ -12,7 +11,9 @@ extern "C" {
 #endif
 
 /* both the omp version and ompacc version */
-extern void axpy(REAL* x, REAL* y,  long n, REAL a); 
+extern void axpy_omp(REAL* x, REAL* y,  long n, REAL a); 
+extern void axpy_ompacc(REAL* x, REAL* y,  long n, REAL a); 
+extern void axpy_ompacc_mdev_1(REAL* x, REAL* y,  long n, REAL a);
 extern double axpy_ompacc_mdev_v2(REAL* x, REAL* y,  long n, REAL a);
 #ifdef __cplusplus
  }
