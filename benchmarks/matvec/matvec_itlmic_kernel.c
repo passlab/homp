951bd0885193e61043da27b21f362f435e24e3ff
Yonghong Yan <yanyh15@gmail.com>
2016-01-13 07:57:22 -0500

    bug fix for pointer issue for itlmic: we need icc to build home_dev.c file. It used nvcc before.

--- benchmarks/matvec/matvec_itlmic_kernel.c
+++ benchmarks/matvec/matvec_itlmic_kernel.c
@@ -19,19 +19,20 @@ void matvec_itlmic_wrapper(omp_offloading_t *off, long n, long start_n, long len
                 y[i] += a[i*n + j] * x[j];
             //printf ("error part!!");
         }
-
     }
 
 #if 0
+    omp_dev_stream_t *stream = off->stream;
+    omp_offloading_info_t * off_info = off->off_info;
     omp_event_t *events = off->events;
 
 #if defined (OMP_BREAKDOWN_TIMING)
     omp_event_record_start(&events[acc_mapto_event_index], stream, "ACC_MAPTO", "Accumulated time for mapto data movement for all array");
 #endif
 
-#pragma offload target(mic) in (x: length(length_n-start_n) alloc_if(1) free_if(0)) \
-                            in (y: length(length_n-start_n) alloc_if(1) free_if(0)) \
-                            in (a: length(length_n-start_n) alloc_if(1) free_if(0))
+#pragma offload target(mic) in (x: length(length_n*sizeof(REAL)) alloc_if(1) free_if(0)) \
+                            in (y: length(length_n*sizeof(REAL)) alloc_if(1) free_if(0)) \
+                            in (a: length(length_n*n*sizeof(REAL)) alloc_if(1) free_if(0))
     {
     }
 #if defined (OMP_BREAKDOWN_TIMING)
@@ -39,10 +40,10 @@ void matvec_itlmic_wrapper(omp_offloading_t *off, long n, long start_n, long len
     omp_event_record_start(&events[acc_kernel_exe_event_index], stream, "KERN", "Time for kernel (%s) execution", off_info->name);
 #endif
 
-#pragma offload target(mic) nocopy (x: length(length_n-start_n) alloc_if(0) free_if(0)) \
-                            nocopy (y: length(length_n-start_n) alloc_if(0) free_if(0)) \
-                            nocopy (a: length(length_n-start_n) alloc_if(0) free_if(0))
-#pragma omp parallel for simd
+#pragma offload target(mic) in (x: length(0) alloc_if(0) free_if(0)) \
+                            in (y: length(0) alloc_if(0) free_if(0)) \
+                            in (a: length(0) alloc_if(0) free_if(0))
+//#pragma omp parallel for simd
     {
 //#pragma omp parallel for shared(y, x, a, start_n, length_n) private(i,j)
         for (i = start_n; i < start_n + length_n; i++) {
@@ -57,9 +58,9 @@ void matvec_itlmic_wrapper(omp_offloading_t *off, long n, long start_n, long len
     omp_event_record_start(&events[acc_mapfrom_event_index], stream,  "ACC_MAPFROM", "Accumulated time for mapfrom data movement for all array");
 #endif
 
-#pragma offload target(mic) nocopy (x: length(length_n-start_n) alloc_if(0) free_if(1)) \
-                            nocopy (y: length(length_n-start_n) alloc_if(0) free_if(1)) \
-                            nocopy (a: length(length_n-start_n) alloc_if(0) free_if(1))
+#pragma offload target(mic) nocopy (x: length(0) alloc_if(0) free_if(1)) \
+                            out (y: length(0) alloc_if(0) free_if(1)) \
+                            nocopy (a: length(0) alloc_if(0) free_if(1))
     {
     }
 #if defined (OMP_BREAKDOWN_TIMING)
